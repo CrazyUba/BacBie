aff31125fd02a5cd963f200d65d39f95fcfc8a91
Ulrich Backes <backes@solthor.de>
2017-02-21 18:01:43 +0100

    Footer added

--- bacbie/Scripts/facebook.js
+++ bacbie/Scripts/facebook.js
@@ -5,4 +5,4 @@
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.8";
     fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));   
